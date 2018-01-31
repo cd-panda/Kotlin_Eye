4a8b2295a10bc6c84dce17e2184719428e3e8148
lipeisheng <lipeisheng@pand-auto.com>
2018-01-31 12:14:11 +0800

    init

---
+++ app/src/main/java/com/pand/kotlin_eye/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.pand.kotlin_eye
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
