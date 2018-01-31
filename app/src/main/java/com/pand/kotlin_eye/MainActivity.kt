a04d2ec32d6c1415e6f3482f3eccdfeabb54a479
lipeisheng <lipeisheng@pand-auto.com>
2018-01-31 17:31:34 +0800

    启动页

--- app/src/main/java/com/pand/kotlin_eye/MainActivity.kt
+++ app/src/main/java/com/pand/kotlin_eye/MainActivity.kt
@@ -5,6 +5,8 @@ import com.pand.kotlin_eye.base.BaseActivity
 class MainActivity : BaseActivity() {
     override fun getLayoutRes() = R.layout.activity_main
     
-    override fun initData() {}
+    override fun initData() {
+
+    }
 
 }
