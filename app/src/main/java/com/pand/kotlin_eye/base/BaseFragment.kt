ba28084b05763f5930daeea4470b726f89ca340c
lipeisheng <lipeisheng@pand-auto.com>
2018-02-01 10:42:32 +0800

    BaseFragment修改

--- app/src/main/java/com/pand/kotlin_eye/base/BaseFragment.kt
+++ app/src/main/java/com/pand/kotlin_eye/base/BaseFragment.kt
@@ -1,16 +1,31 @@
 package com.pand.kotlin_eye.base
 
+import android.databinding.DataBindingUtil
+import android.databinding.ViewDataBinding
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.pand.kotlin_eye.mvvm.viewmodel.BaseViewModel
 
 /**
  * Created by 李培生 on 2018/1/31 18:06.
  */
-abstract class BaseFragment : Fragment() {
-    val TAG=this::class.java.simpleName
-    override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View? = inflater?.inflate(getLayoutRes(), container)
+abstract class BaseFragment<T : BaseViewModel, D : ViewDataBinding> : Fragment() {
+    val TAG = this::class.java.simpleName
+    lateinit var mViewModel: T
+    lateinit var mBinding: D
+    override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        mBinding = DataBindingUtil.inflate(inflater, getLayoutRes(), container, false)
+        getViewModel()
+        setViewModel()
+        return mBinding.root
+    }
+    
+    abstract fun setViewModel()
+    
+    abstract fun getViewModel()
+    
     abstract fun getLayoutRes(): Int
 }
