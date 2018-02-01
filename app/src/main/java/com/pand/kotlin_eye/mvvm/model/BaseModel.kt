af68c8bbef3432450fda2f6a8a91008514d1818e
lipeisheng <lipeisheng@pand-auto.com>
2018-02-01 10:05:45 +0800

    MVVM基

---
+++ app/src/main/java/com/pand/kotlin_eye/mvvm/model/BaseModel.kt
@@ -0,0 +1,6 @@
+package com.pand.kotlin_eye.mvvm.model
+
+/**
+ * Created by 李培生 on 2018/2/1 10:03.
+ */
+open  class BaseModel{}
---
+++ app/src/main/java/com/pand/kotlin_eye/mvvm/viewmodel/BaseViewModel.kt
@@ -0,0 +1,6 @@
+package com.pand.kotlin_eye.mvvm.viewmodel
+
+/**
+ * Created by 李培生 on 2018/2/1 10:02.
+ */
+open class BaseViewModel{}
